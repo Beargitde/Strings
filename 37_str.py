29ed7733c13d28a241529514e9140b1b58ee71e2
Beargitde <56301790+Beargitde@users.noreply.github.com>
2019-10-25 16:55:06 +0300

    Add files via upload
    CORRECTED

---
+++ 37_str.py
@@ -0,0 +1,12 @@
+strin = input('Введите строку:')
+strin = list(strin)
+a = len(strin)
+for x in range (1,3):
+    print(strin, a)
+    print('символов, ')
+for x in range (0,1):
+    print(strin, a)
+    print('символов. ')
+
+
+
